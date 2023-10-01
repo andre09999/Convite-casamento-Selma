9e95c557afd446a540e1ad6500f2cc37c2593759
Andre <andre@pop-os.localdomain>
2023-10-01 15:10:35 -0300

    versão final

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import BelaFera from './images/rosa.png'
 import './App.css'
 function header() {
 
   return (
     <header>
-            <img alt='FOto bela e a fera' src={BelaFera}/>
-            <p className='versiculo'>Para que todos vejam, e saibam, e considerem, e juntamente entendam que a mão do Senhor fez isto, e o Santo de Israel o criou.
-Isaías 41:20</p>
-            <img alt='FOto bela e a fera' src={BelaFera}/>
+            <p className='versiculo'>
+Se você amar alguém, será real para com ele, custe o que custar. Sempre acreditará nele, sempre esperará o melhor dele, e sempre se manterá em sua defesa. 1 Coríntios 13:7 (VIVA) </p>
     </header>
   );
 }
